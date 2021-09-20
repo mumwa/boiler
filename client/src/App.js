51d18fd30cdd81748ac0e41d43e7a9e06bbd192d
mumwa <mumwa@naver.com>
2021-09-20 18:03:00 +0900

    Fix cors

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -16,25 +15,12 @@ import RegisterPage from './components/views/RegisterPage/RegisterPage'
 function App() {
   return (
     <Router>
-    <div>
-      {/*
-        A <Switch> looks through all its children <Route>
-        elements and renders the first one whose path
-        matches the current URL. Use a <Switch> any time
-        you have multiple routes, but you want only one
-        of them to render at a time
-      */}
       <Switch>
-        <Route exact path="/" component={LandingPage}/>
-        <Route exact path="/login" component={LoginPage}/>
-        <Route exact path="/register" component={RegisterPage}/>
-        <Route exact path="/navbar" component={NavBar}/>
-        <Route exact path="/Footer">
-          <Footer/>
-        </Route>
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/register" component={RegisterPage} />
       </Switch>
-    </div>
-  </Router>
+    </Router>
   );
 }
 
