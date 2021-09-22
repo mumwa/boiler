9654eff45227a4dffbc030619e1ef8343771c96a
mumwa <mumwa@naver.com>
2021-09-22 23:39:57 +0900

    Add reducer settings

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,31 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'antd/dist/antd.css';
+
+import {applyMiddleware, createStore} from 'redux';
+import {Provider} from 'react-redux';
+import promiseMiddleware from 'redux-promise';
+import reduxThunk from 'redux-thunk';
+
+import Reducer from './_reducers';
+
+
+
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, reduxThunk)(createStore);
+
 ReactDOM.render(
+  <Provider
+    store={createStoreWithMiddleware(Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__&&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}
+  >
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
